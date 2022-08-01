5307e4fe6ef9a8fc787c379325abf595a7df2663
Vishal Gupta <vishal.gupta@valtech.com>
2022-08-01 12:11:57 +0100

    EIP1-1210: WebTestClient response timeout set to 5 minutes.

--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/IntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/IntegrationTest.kt
@@ -1,26 +1,19 @@
 package uk.gov.dluhc.registercheckerapi.config
 
-import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
-import java.time.Duration
 
 /**
  * Base class used to bring up the entire Spring ApplicationContext
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@AutoConfigureWebTestClient(timeout = "PT5M")
 internal abstract class IntegrationTest {
 
     @Autowired
     protected lateinit var webTestClient: WebTestClient
-
-    @BeforeEach
-    fun customizeWebTestClient() {
-        webTestClient = webTestClient.mutate()
-            .responseTimeout(Duration.ofSeconds(300))
-            .build()
-    }
 }

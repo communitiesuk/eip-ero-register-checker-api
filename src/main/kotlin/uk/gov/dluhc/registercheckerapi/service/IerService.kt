caf2c24c73193fbf05d338a150f48fd18e8ca01f
Vishal Gupta <vishal.gupta@valtech.com>
2022-08-02 13:18:59 +0100

    EIP1-1172: Removed unused logger

--- src/main/kotlin/uk/gov/dluhc/registercheckerapi/service/IerService.kt
+++ src/main/kotlin/uk/gov/dluhc/registercheckerapi/service/IerService.kt
@@ -1,12 +1,9 @@
 package uk.gov.dluhc.registercheckerapi.service
 
-import mu.KotlinLogging
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import uk.gov.dluhc.registercheckerapi.client.IerApiClient
 import uk.gov.dluhc.registercheckerapi.config.CachingConfig
-
-private val logger = KotlinLogging.logger {}
 
 @Service
 class IerService(private val ierApiClient: IerApiClient) {

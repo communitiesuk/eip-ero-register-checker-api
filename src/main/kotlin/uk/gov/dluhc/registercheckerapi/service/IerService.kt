9b6b659b031975852245f17551912bf72f018899
Vishal Gupta <vishal.gupta@valtech.com>
2022-08-02 10:43:19 +0100

    EIP1-1171: Removed redundant code

--- src/main/kotlin/uk/gov/dluhc/registercheckerapi/service/IerService.kt
+++ src/main/kotlin/uk/gov/dluhc/registercheckerapi/service/IerService.kt
@@ -1,10 +1,7 @@
 package uk.gov.dluhc.registercheckerapi.service
 
-import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import uk.gov.dluhc.registercheckerapi.client.IerApiClient
-
-private val logger = KotlinLogging.logger {}
 
 @Service
 class IerService(private val ierApiClient: IerApiClient) {

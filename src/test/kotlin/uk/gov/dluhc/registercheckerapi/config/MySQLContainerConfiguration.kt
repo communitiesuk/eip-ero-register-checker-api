bd1052065c56439c50109ce1ccd3db6f6aac7ad0
Matt Wills <matt.wills@nationaltrust.org.uk>
2022-09-12 11:52:44 +0100

    EIP1-1750 - Add DB configuration, along with empty liquibase setup

---
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/MySQLContainerConfiguration.kt
@@ -0,0 +1,27 @@
+package uk.gov.dluhc.registercheckerapi.config
+
+import org.testcontainers.containers.MySQLContainer
+
+class MySQLContainerConfiguration : MySQLContainer<MySQLContainerConfiguration>(MYSQL_IMAGE) {
+    companion object {
+        private const val MYSQL_IMAGE = "mysql:8"
+        private const val DATABASE = "register_checker_application"
+        private const val USER = "root"
+        private const val PASSWORD = "password"
+        private const val DATASOURCE_URL = "spring.datasource.url"
+        private var container: MySQLContainerConfiguration? = null
+
+        fun getInstance(): MySQLContainerConfiguration {
+            if (container == null) {
+                container = MySQLContainerConfiguration().withDatabaseName(DATABASE)
+                    .withUsername(USER)
+                    .withPassword(PASSWORD)
+                    .also {
+                        it.start()
+                        System.setProperty(DATASOURCE_URL, it.jdbcUrl)
+                    }
+            }
+            return container!!
+        }
+    }
+}

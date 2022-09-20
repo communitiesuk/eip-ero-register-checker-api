21a743d71e09d23c419251aa55abd271ca9e716a
Rob O'Rorke <rob.o'rorke@valtech.com>
2022-09-20 14:24:14 +0100

    EIP1-1730: EIP1-1754: add message listener

--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/LocalstackMessagingConfiguration.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/LocalstackMessagingConfiguration.kt
@@ -2,7 +2,6 @@ package uk.gov.dluhc.registercheckerapi.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.DependsOn
 import org.springframework.context.annotation.Primary
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
 import software.amazon.awssdk.services.sqs.SqsClient
@@ -12,7 +11,6 @@ import java.net.URI
 class LocalstackMessagingConfiguration {
     @Bean
     @Primary
-    @DependsOn("localStackContainerSqsSettings")
     fun testSqsClient(awsBasicCredentialsProvider: AwsCredentialsProvider, localStackContainerSqsSettings: LocalStackContainerSettings): SqsClient =
         SqsClient.builder()
             .credentialsProvider(awsBasicCredentialsProvider)

cc44d9c2144d2d70f62f562ba4ae3343d948aa21
Vishal Gupta <vishal.gupta@valtech.com>
2022-09-21 13:58:39 +0100

    Addresses review comments - Class renamed

--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/testsupport/IdTestDataFactory.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/testsupport/IdTestDataFactory.kt
@@ -1,6 +1,5 @@
 package uk.gov.dluhc.registercheckerapi.testsupport
 
-import org.apache.commons.lang3.RandomStringUtils.randomNumeric
 import uk.gov.dluhc.registercheckerapi.testsupport.testdata.DataFaker.Companion.faker
 
 fun getRandomEroId() = "${faker.address().city().lowercase()}-city-council"
@@ -13,4 +12,4 @@ fun getDifferentRandomEroId(refEroId: String): String {
     return differentEroId
 }
 
-fun getRandomGssCode() = "E${randomNumeric(8)}"
+fun getRandomGssCode() = "E${faker.random().nextLong(8)}"

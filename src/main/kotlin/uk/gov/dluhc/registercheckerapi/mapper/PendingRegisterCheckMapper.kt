324e9e98933eb0e4917543756c1275d8133cc470
Matt Wills <matt.wills@nationaltrust.org.uk>
2022-09-22 20:53:27 +0100

    EIP1-1858 Mapping of DTO to API Model

--- src/main/kotlin/uk/gov/dluhc/registercheckerapi/mapper/PendingRegisterCheckMapper.kt
+++ src/main/kotlin/uk/gov/dluhc/registercheckerapi/mapper/PendingRegisterCheckMapper.kt
@@ -2,11 +2,15 @@ package uk.gov.dluhc.registercheckerapi.mapper
 
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.ValueMapping
 import uk.gov.dluhc.registercheckerapi.database.entity.PersonalDetail
 import uk.gov.dluhc.registercheckerapi.database.entity.RegisterCheck
 import uk.gov.dluhc.registercheckerapi.dto.PendingRegisterCheckDto
 import uk.gov.dluhc.registercheckerapi.dto.PersonalDetailDto
+import uk.gov.dluhc.registercheckerapi.dto.SourceType
 import uk.gov.dluhc.registercheckerapi.messaging.models.InitiateRegisterCheckMessage
+import uk.gov.dluhc.registercheckerapi.models.PendingRegisterCheck
+import uk.gov.dluhc.registercheckerapi.models.SourceSystem
 
 /**
  * Maps incoming [InitiateRegisterCheckMessage] to [PendingRegisterCheckDto]. Maps the entity class [RegisterCheck]
@@ -25,9 +29,32 @@ interface PendingRegisterCheckMapper {
     @Mapping(target = "createdAt", source = "dateCreated")
     fun registerCheckEntityToPendingRegisterCheckDto(registerCheck: RegisterCheck): PendingRegisterCheckDto
 
+    @Mapping(target = "requestid", source = "correlationId")
+    @Mapping(target = "source", source = "sourceType")
+    @Mapping(target = "gssCode", source = "gssCode")
+    @Mapping(target = "actingStaffId", constant = "EROP") // TODO - will contain userId when manual checks are allowed in future
+    @Mapping(target = "fn", source = "pendingRegisterCheckDto.personalDetail.firstName")
+    @Mapping(target = "mn", source = "pendingRegisterCheckDto.personalDetail.middleNames")
+    @Mapping(target = "ln", source = "pendingRegisterCheckDto.personalDetail.surname")
+    @Mapping(target = "dob", source = "pendingRegisterCheckDto.personalDetail.dateOfBirth")
+    @Mapping(target = "phone", source = "pendingRegisterCheckDto.personalDetail.phone")
+    @Mapping(target = "email", source = "pendingRegisterCheckDto.personalDetail.email")
+    @Mapping(target = "regproperty", source = "pendingRegisterCheckDto.personalDetail.address.property")
+    @Mapping(target = "regstreet", source = "pendingRegisterCheckDto.personalDetail.address.street")
+    @Mapping(target = "regpostcode", source = "pendingRegisterCheckDto.personalDetail.address.postcode")
+    @Mapping(target = "reglocality", source = "pendingRegisterCheckDto.personalDetail.address.locality")
+    @Mapping(target = "regtown", source = "pendingRegisterCheckDto.personalDetail.address.town")
+    @Mapping(target = "regarea", source = "pendingRegisterCheckDto.personalDetail.address.area")
+    @Mapping(target = "reguprn", source = "pendingRegisterCheckDto.personalDetail.address.uprn")
+    @Mapping(target = "createdAt", expression = "java(pendingRegisterCheckDto.getCreatedAt().atOffset(java.time.ZoneOffset.UTC))")
+    fun pendingRegisterCheckDtoToPendingRegisterCheckModel(pendingRegisterCheckDto: PendingRegisterCheckDto): PendingRegisterCheck
+
     @Mapping(target = "phoneNumber", source = "phone")
     fun personalDetailDtoToPersonalDetailEntity(personalDetailDto: PersonalDetailDto): PersonalDetail
 
     @Mapping(target = "phone", source = "phoneNumber")
     fun personalDetailEntityToPersonalDetailDto(personalDetail: PersonalDetail): PersonalDetailDto
+
+    @ValueMapping(source = "VOTER_CARD", target = "EROP")
+    fun sourceTypeToSourceSystem(sourceType: SourceType): SourceSystem
 }
--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/testsupport/testdata/dto/RegisterCheckBuilder.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/testsupport/testdata/dto/RegisterCheckBuilder.kt
@@ -3,6 +3,7 @@ package uk.gov.dluhc.registercheckerapi.testsupport.testdata.dto
 import uk.gov.dluhc.registercheckerapi.dto.PendingRegisterCheckDto
 import uk.gov.dluhc.registercheckerapi.dto.PersonalDetailDto
 import uk.gov.dluhc.registercheckerapi.dto.SourceType
+import java.time.Instant
 import java.util.UUID
 
 fun buildPendingRegisterCheckDto(
@@ -12,7 +13,8 @@ fun buildPendingRegisterCheckDto(
     sourceType: SourceType = SourceType.VOTER_CARD,
     gssCode: String = "E09000021",
     personalDetail: PersonalDetailDto = buildPersonalDetailDto(),
-    createdBy: String = "system"
+    createdBy: String = "system",
+    createdAt: Instant? = null
 ) = PendingRegisterCheckDto(
     correlationId = correlationId,
     sourceReference = sourceReference,
@@ -20,5 +22,6 @@ fun buildPendingRegisterCheckDto(
     sourceType = sourceType,
     gssCode = gssCode,
     personalDetail = personalDetail,
-    createdBy = createdBy
+    createdBy = createdBy,
+    createdAt = createdAt
 )

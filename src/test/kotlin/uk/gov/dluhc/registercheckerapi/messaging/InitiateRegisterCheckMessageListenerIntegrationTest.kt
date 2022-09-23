4e70c698e4f637e051d03ab26e0eaceb560918a7
Rob O'Rorke <56517757+sentioservices@users.noreply.github.com>
2022-09-23 08:59:02 +0100

    EIP1-1730: EIP1-1754: add RegisterCheckService impl (#36)
    * EIP1-1730: EIP1-1754: add RegisterCheckService impl

--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/messaging/InitiateRegisterCheckMessageListenerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/messaging/InitiateRegisterCheckMessageListenerIntegrationTest.kt
@@ -2,13 +2,23 @@ package uk.gov.dluhc.registercheckerapi.messaging
 
 import mu.KotlinLogging
 import org.apache.commons.lang3.time.StopWatch
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.verify
 import org.testcontainers.shaded.org.awaitility.Awaitility.await
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest
 import uk.gov.dluhc.registercheckerapi.config.IntegrationTest
+import uk.gov.dluhc.registercheckerapi.database.entity.Address
+import uk.gov.dluhc.registercheckerapi.database.entity.CheckStatus
+import uk.gov.dluhc.registercheckerapi.database.entity.PersonalDetail
+import uk.gov.dluhc.registercheckerapi.database.entity.RegisterCheck
+import uk.gov.dluhc.registercheckerapi.database.entity.SourceType
+import uk.gov.dluhc.registercheckerapi.messaging.models.InitiateRegisterCheckMessage
+import uk.gov.dluhc.registercheckerapi.testsupport.assertj.assertions.RegisterCheckAssert
 import uk.gov.dluhc.registercheckerapi.testsupport.testdata.models.buildInitiateRegisterCheckMessage
+import java.time.Instant
+import java.util.UUID
 import java.util.concurrent.TimeUnit
+import javax.persistence.criteria.CriteriaQuery
 
 private val logger = KotlinLogging.logger {}
 
@@ -19,6 +29,34 @@ internal class InitiateRegisterCheckMessageListenerIntegrationTest : Integration
         // Given
         val message = buildInitiateRegisterCheckMessage()
         val payload = objectMapper.writeValueAsString(message)
+        val earliestDateCreated = Instant.now()
+        val expected = RegisterCheck(
+            correlationId = UUID.randomUUID(),
+            sourceType = SourceType.VOTER_CARD,
+            sourceReference = message.sourceReference,
+            sourceCorrelationId = message.sourceCorrelationId,
+            createdBy = message.requestedBy,
+            gssCode = message.gssCode,
+            status = CheckStatus.PENDING,
+            version = 0L,
+            personalDetail = PersonalDetail(
+                firstName = message.personalDetail.firstName,
+                middleNames = message.personalDetail.middleNames,
+                surname = message.personalDetail.surname,
+                dateOfBirth = message.personalDetail.dateOfBirth,
+                phoneNumber = message.personalDetail.phone,
+                email = message.personalDetail.email,
+                address = Address(
+                    property = message.personalDetail.address.property,
+                    street = message.personalDetail.address.street,
+                    locality = message.personalDetail.address.locality,
+                    town = message.personalDetail.address.town,
+                    area = message.personalDetail.address.area,
+                    postcode = message.personalDetail.address.postcode,
+                    uprn = message.personalDetail.address.uprn,
+                )
+            )
+        )
 
         // When
         sqsClient.sendMessage(
@@ -31,10 +69,35 @@ internal class InitiateRegisterCheckMessageListenerIntegrationTest : Integration
         // Then
         val stopWatch = StopWatch.createStarted()
         await().atMost(5, TimeUnit.SECONDS).untilAsserted {
-            verify(registerCheckService).initiateRegisterCheck()
+            val actualRegisterCheckJpaEntity = getActualRegisterCheckJpaEntity(message)
+            logger.info("found actualRegisterCheckJpaEntitys[${actualRegisterCheckJpaEntity.size}]")
+            Assertions.assertThat(actualRegisterCheckJpaEntity).hasSize(1)
+
+            RegisterCheckAssert.assertThat(actualRegisterCheckJpaEntity.first())
+                .ignoringIdFields()
+                .ignoringDateFields()
+                .isRecursivelyEqual(expected)
+                .hasIdAndDbAuditFieldsAfter(earliestDateCreated)
 
             stopWatch.stop()
             logger.info("completed assertions in $stopWatch")
+        }
+    }
+
+    private fun getActualRegisterCheckJpaEntity(message: InitiateRegisterCheckMessage): List<RegisterCheck> {
+        val entityManager = entityManagerFactory.createEntityManager()
+        try {
+            val cb = entityManager.criteriaBuilder
+            val queryDef: CriteriaQuery<RegisterCheck> = cb.createQuery(RegisterCheck::class.java)
+            val rc = queryDef.from(RegisterCheck::class.java)
+            queryDef.select(
+                rc
+            ).where(
+                cb.equal(rc.get<UUID>("sourceCorrelationId"), message.sourceCorrelationId)
+            )
+            return entityManager.createQuery(queryDef).resultList
+        } finally {
+            entityManager.close()
         }
     }
 }
---
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/testsupport/assertj/assertions/RegisterCheckAssert.kt
@@ -0,0 +1,76 @@
+package uk.gov.dluhc.registercheckerapi.testsupport.assertj.assertions
+
+import mu.KotlinLogging
+import org.assertj.core.api.AbstractAssert
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.InstanceOfAssertFactories
+import uk.gov.dluhc.registercheckerapi.database.entity.RegisterCheck
+import java.time.Instant
+
+private val logger = KotlinLogging.logger { }
+
+class RegisterCheckAssert(private val actualJpaEntity: RegisterCheck?) :
+    AbstractAssert<RegisterCheckAssert, RegisterCheck?>(
+        actualJpaEntity,
+        RegisterCheckAssert::class.java
+    ) {
+    private var ignoringFields = arrayOf<String>()
+
+    companion object {
+        private val ID_FIELDS = arrayOf(
+            "id",
+            "personalDetail.id",
+            "personalDetail.address.id",
+            "correlationId",
+        )
+        private val DATE_FIELDS = arrayOf(
+            "dateCreated",
+            "personalDetail.dateCreated",
+            "personalDetail.address.dateCreated",
+            "updatedAt",
+        )
+
+        fun assertThat(actual: RegisterCheck?): RegisterCheckAssert {
+            return RegisterCheckAssert(actual)
+        }
+    }
+
+    fun isRecursivelyEqual(expectedJpaEntity: RegisterCheck?): RegisterCheckAssert {
+        try {
+            Assertions.assertThat(actualJpaEntity)
+                .usingRecursiveComparison()
+                .ignoringFields(*ignoringFields)
+                .isEqualTo(expectedJpaEntity)
+        } catch (e: Error) {
+            logger.debug("failed to assert: ", e)
+            throw e
+        }
+        return this
+    }
+
+    fun hasIdAndDbAuditFieldsAfter(earliestInstant: Instant) {
+        Assertions.assertThat(actual!!.id!!).isNotNull
+        Assertions.assertThat(actual!!.personalDetail.id!!).isNotNull
+        Assertions.assertThat(actual!!.personalDetail.address.id!!).isNotNull
+
+        Assertions.assertThat(actual!!)
+            .extracting { it.dateCreated!! }
+            .asInstanceOf(InstanceOfAssertFactories.INSTANT)
+            .isAfter(earliestInstant)
+
+        Assertions.assertThat(actual!!)
+            .extracting { it.updatedAt!! }
+            .asInstanceOf(InstanceOfAssertFactories.INSTANT)
+            .isAfter(earliestInstant)
+    }
+
+    fun ignoringIdFields(): RegisterCheckAssert {
+        ignoringFields += ID_FIELDS
+        return this
+    }
+
+    fun ignoringDateFields(): RegisterCheckAssert {
+        ignoringFields += DATE_FIELDS
+        return this
+    }
+}

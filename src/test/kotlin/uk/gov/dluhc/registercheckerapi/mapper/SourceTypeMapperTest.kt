f89d90ba3de1d3f01fa631416836b78d7b39b053
Vishal Gupta <vishal.gupta@valtech.com>
2023-01-10 18:50:49 +0000

    Moves sourceType related method to SourceTypeMapper

--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/mapper/SourceTypeMapperTest.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/mapper/SourceTypeMapperTest.kt
@@ -1,12 +1,15 @@
 package uk.gov.dluhc.registercheckerapi.mapper
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import uk.gov.dluhc.registercheckerapi.messaging.models.RegisterCheckSourceType
+import uk.gov.dluhc.registercheckerapi.models.SourceSystem
 import uk.gov.dluhc.registercheckerapi.database.entity.SourceType as SourceTypeEntityEnum
 import uk.gov.dluhc.registercheckerapi.dto.SourceType as SourceTypeDtoEnum
 import uk.gov.dluhc.registercheckerapi.messaging.models.RegisterCheckSourceType as SourceTypeSqsEnum
+
 class SourceTypeMapperTest {
     private val mapper = SourceTypeMapperImpl()
 
@@ -65,5 +68,16 @@ class SourceTypeMapperTest {
 
         // Then
         Assertions.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should map DTO Source Type enum to Source system`() {
+        // Given
+
+        // When
+        val actual = mapper.sourceTypeDtoToSourceSystem(SourceTypeDtoEnum.VOTER_CARD)
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(SourceSystem.EROP)
     }
 }

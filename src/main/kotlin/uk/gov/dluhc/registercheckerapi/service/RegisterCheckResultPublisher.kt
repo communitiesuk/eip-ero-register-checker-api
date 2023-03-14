1277d467b24e6a079cf10249145286dfabe903e9
vinay <vinay.hosamani@valtech.com>
2023-03-14 11:40:30 +0000

    update mapper test

--- src/main/kotlin/uk/gov/dluhc/registercheckerapi/service/RegisterCheckResultPublisher.kt
+++ src/main/kotlin/uk/gov/dluhc/registercheckerapi/service/RegisterCheckResultPublisher.kt
@@ -3,6 +3,7 @@ package uk.gov.dluhc.registercheckerapi.service
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import uk.gov.dluhc.applicationsapi.messaging.models.RegisterCheckResultMessage
+import uk.gov.dluhc.applicationsapi.messaging.models.SourceType
 import uk.gov.dluhc.applicationsapi.messaging.models.SourceType.OVERSEAS_MINUS_VOTE
 import uk.gov.dluhc.applicationsapi.messaging.models.SourceType.POSTAL_MINUS_VOTE
 import uk.gov.dluhc.applicationsapi.messaging.models.SourceType.PROXY_MINUS_VOTE
@@ -19,11 +20,14 @@ class RegisterCheckResultPublisher(
 ) : MessagePublisher<RegisterCheckResultMessage> {
 
     override fun publish(payload: RegisterCheckResultMessage) {
-        when (payload.sourceType) {
+        getTargetQueueForSourceType(payload.sourceType).submit(payload)
+    }
+
+    private fun getTargetQueueForSourceType(sourceType: SourceType) =
+        when (sourceType) {
             VOTER_MINUS_CARD -> confirmRegisterCheckResultQueue
             POSTAL_MINUS_VOTE -> postalVoteConfirmRegisterCheckResultQueue
             PROXY_MINUS_VOTE -> proxyVoteConfirmRegisterCheckResultQueue
             OVERSEAS_MINUS_VOTE -> overseasVoteConfirmRegisterCheckResultQueue
-        }.submit(payload)
-    }
+        }
 }
--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/mapper/SourceTypeMapperTest.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/mapper/SourceTypeMapperTest.kt
@@ -1,7 +1,6 @@
 package uk.gov.dluhc.registercheckerapi.mapper
 
 import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import uk.gov.dluhc.registercheckerapi.models.SourceSystem
@@ -16,6 +15,9 @@ class SourceTypeMapperTest {
     @CsvSource(
         value = [
             "VOTER_MINUS_CARD, VOTER_CARD",
+            "POSTAL_MINUS_VOTE, POSTAL_VOTE",
+            "PROXY_MINUS_VOTE, PROXY_VOTE",
+            "OVERSEAS_MINUS_VOTE, OVERSEAS_VOTE",
         ]
     )
     fun `should map Message Source Type enum to DTO Source Type`(
@@ -35,6 +37,9 @@ class SourceTypeMapperTest {
     @CsvSource(
         value = [
             "VOTER_CARD, VOTER_MINUS_CARD",
+            "POSTAL_VOTE, POSTAL_MINUS_VOTE",
+            "PROXY_VOTE, PROXY_MINUS_VOTE",
+            "OVERSEAS_VOTE, OVERSEAS_MINUS_VOTE",
         ]
     )
     fun `should map Entity Source Type to VCA Message Source Type`(
@@ -54,6 +59,9 @@ class SourceTypeMapperTest {
     @CsvSource(
         value = [
             "VOTER_CARD, VOTER_CARD",
+            "POSTAL_VOTE, POSTAL_VOTE",
+            "PROXY_VOTE, PROXY_VOTE",
+            "OVERSEAS_VOTE, OVERSEAS_VOTE",
         ]
     )
     fun `should map Entity Source Type enum to DTO Source Type`(
@@ -73,6 +81,9 @@ class SourceTypeMapperTest {
     @CsvSource(
         value = [
             "VOTER_CARD, VOTER_CARD",
+            "POSTAL_VOTE, POSTAL_VOTE",
+            "PROXY_VOTE, PROXY_VOTE",
+            "OVERSEAS_VOTE, OVERSEAS_VOTE",
         ]
     )
     fun `should map DTO Source Type enum to Entity Source Type`(
@@ -88,14 +99,25 @@ class SourceTypeMapperTest {
         Assertions.assertThat(actual).isEqualTo(expected)
     }
 
-    @Test
-    fun `should map DTO Source Type enum to Source system`() {
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "VOTER_CARD, EROP",
+            "POSTAL_VOTE, EROP",
+            "PROXY_VOTE, EROP",
+            "OVERSEAS_VOTE, EROP",
+        ]
+    )
+    fun `should map DTO Source Type enum to Source system`(
+        sourceType: SourceTypeDtoEnum,
+        expected: SourceSystem
+    ) {
         // Given
 
         // When
-        val actual = mapper.sourceTypeDtoToSourceSystem(SourceTypeDtoEnum.VOTER_CARD)
+        val actual = mapper.sourceTypeDtoToSourceSystem(sourceType)
 
         // Then
-        Assertions.assertThat(actual).isEqualTo(SourceSystem.EROP)
+        Assertions.assertThat(actual).isEqualTo(expected)
     }
 }

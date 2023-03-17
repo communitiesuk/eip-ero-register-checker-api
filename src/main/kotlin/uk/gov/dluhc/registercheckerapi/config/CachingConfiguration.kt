535dea3080171a3b4e09bf72a7dba241bbde1143
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-03-17 09:51:18 +0000

    EIP1-0000 - Replaced guava with caffeine cache manager (#101)

--- src/main/kotlin/uk/gov/dluhc/registercheckerapi/config/CachingConfiguration.kt
+++ src/main/kotlin/uk/gov/dluhc/registercheckerapi/config/CachingConfiguration.kt
@@ -1,13 +1,10 @@
 package uk.gov.dluhc.registercheckerapi.config
 
-import com.google.common.cache.CacheBuilder
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.Cache
 import org.springframework.cache.CacheManager
-import org.springframework.cache.annotation.CachingConfigurerSupport
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.concurrent.ConcurrentMapCache
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.time.Duration
@@ -16,21 +13,16 @@ const val ERO_CERTIFICATE_MAPPING_CACHE = "eroCertificateMappings"
 
 @Configuration
 @EnableCaching
-class CachingConfiguration : CachingConfigurerSupport() {
+class CachingConfiguration {
     @Value("\${caching.time-to-live}")
     private lateinit var timeToLive: Duration
 
     @Bean
-    @Override
-    override fun cacheManager(): CacheManager {
-        val cacheManager = object : ConcurrentMapCacheManager() {
-            override fun createConcurrentMapCache(name: String): Cache {
-                val map = CacheBuilder.newBuilder().expireAfterWrite(timeToLive).build<Any, Any>().asMap()
-                return ConcurrentMapCache(name, map, true)
+    fun cacheManager(): CacheManager {
+        return CaffeineCacheManager()
+            .apply {
+                setCaffeine(Caffeine.newBuilder().expireAfterWrite(timeToLive))
+                setCacheNames(listOf(ERO_CERTIFICATE_MAPPING_CACHE))
             }
-        }
-
-        cacheManager.setCacheNames(listOf(ERO_CERTIFICATE_MAPPING_CACHE))
-        return cacheManager
     }
 }

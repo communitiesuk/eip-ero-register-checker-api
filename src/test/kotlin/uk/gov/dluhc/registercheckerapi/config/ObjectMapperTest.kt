e66bb11f68d2d406057cc1d704b3d8f0eb219ba3
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2023-11-06 09:52:54 +0000

    EROPSPT-190 - Add tests for happy path

--- src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/ObjectMapperTest.kt
+++ src/test/kotlin/uk/gov/dluhc/registercheckerapi/config/ObjectMapperTest.kt
@@ -10,7 +10,7 @@ internal class ObjectMapperTest {
     private val objectMapper = JacksonConfiguration().objectMapper()
 
     @Test
-    fun `should map times without timezone information to OffsetDateTime`() {
+    fun `should map times without offset information to OffsetDateTime`() {
 
         // Given
         val dateTime = "2023-10-28T20:50:07"
@@ -21,6 +21,34 @@ internal class ObjectMapperTest {
 
         // Then
         assertThat(output.offsetDateTime.toLocalDateTime()).isEqualTo(LocalDateTime.parse(dateTime))
+    }
+
+    @Test
+    fun `should map times with default offset information to OffsetDateTime`() {
+
+        // Given
+        val dateTime = "2023-10-28T20:50:07Z"
+        val input = "{\"offsetDateTime\": \"$dateTime\"}"
+
+        // When
+        val output = objectMapper.readValue(input, OffsetDateTimeHolder::class.java)
+
+        // Then
+        assertThat(output.offsetDateTime).isEqualTo(OffsetDateTime.parse(dateTime))
+    }
+
+    @Test
+    fun `should map times with explicit offset information to OffsetDateTime`() {
+
+        // Given
+        val dateTime = "2023-10-28T21:50:07+01:00"
+        val input = "{\"offsetDateTime\": \"$dateTime\"}"
+
+        // When
+        val output = objectMapper.readValue(input, OffsetDateTimeHolder::class.java)
+
+        // Then
+        assertThat(output.offsetDateTime).isEqualTo(OffsetDateTime.parse(dateTime))
     }
 
     data class OffsetDateTimeHolder(val offsetDateTime: OffsetDateTime)

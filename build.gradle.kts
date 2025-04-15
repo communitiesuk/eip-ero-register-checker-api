b12ede74153cb62dd2598cfb834d1f549b55e0bd
Alex Yip <alex.yip@softwire.com>
2025-04-15 15:56:17 +0100

    EROPSPT-443: Downgrade dependencychecker to 12.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.0.0"
     id("org.openapi.generator") version "7.0.1"
-    id("org.owasp.dependencycheck") version "12.1.1"
+    id("org.owasp.dependencycheck") version "12.1.0"
 }
 
 group = "uk.gov.dluhc"
